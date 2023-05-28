e3f56e5d0cc7699a94143270b5ae8eedf80b2e39
KaeHyun <80453200+KaeHyun@users.noreply.github.com>
2023-05-29 04:44:45 +0900

    feat

--- components/scatterPlot.js
+++ components/scatterPlot.js
@@ -1,16 +1,14 @@
 class ScatterPlot{
     
     //SVG 요소 설정
-    margin = { top: 10, right: 20, bottom: 30, left: 30};
+    margin = { top: 20, right: 20, bottom: 100, left: 37};
 
     constructor(data)
     {
         this.data = data;
-        this.width = 400;
-        this.height = 300;
-
+        this.width =600;
+        this.height = 460;
         this.x = d3.scaleLinear().range([0, this.width]);
-
         this.y = d3.scaleLinear().range([this.height, 0]);
 
         this.handler = {};
@@ -19,10 +17,11 @@ class ScatterPlot{
     //초기화
     initialize(){
 
-        this.svg = d3.select("#chart")
+        this.svg = d3.select("#scatterchart")
         .append("svg")
         .attr("width", this.width + this.margin.left + this.margin.right)
         .attr("height", this.height + this.margin.top + this.margin.bottom)
+        .style("position", "absolute") 
         .append("g")
         .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
@@ -38,8 +37,6 @@ class ScatterPlot{
         .attr("class", "axis-label")
         .attr("x", this.width)
         .attr("y", -6)
-        .style("text-anchor", "end")
-        .text("Average Viewrs");
 
         //y축 update
         this.svg.selectAll(".y-axis")
@@ -50,19 +47,20 @@ class ScatterPlot{
         .attr("x", 0)
         .attr("y", 10)
         .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Followers");
+
         // Set up x-axis label
         this.svg.append("text").attr("class", "axis-label")
         .attr("x", this.width).attr("y", this.height - 6)
-        .style("text-anchor", "end").text("Average Viewers");
 
         // Set up y-axis label
         this.svg.append("text").attr("class", "axis-label")
         .attr("transform", "rotate(-90)")
         .attr("x", 0).attr("y", 10).attr("dy", ".71em")
-        .style("text-anchor", "end").text("Followers");
-                
+
+        
+        this.xScale = d3.scaleLinear().range([0, this.width]);
+        this.yScale = d3.scaleLinear().range([this.height, 0]);
+
         this.legend = this.svg.append("g");
 
         this.color = d3.scaleOrdinal(d3.schemeDark2);
@@ -71,37 +69,95 @@ class ScatterPlot{
     }
 
     //선택된 언어에따라 update
-    update(language)
+    update(first, second, xAxisVal, yAxisVal)
     {
-        var filteredData = this.data;
+        var filterdata1 = this.data.filter((d) => d.Language === first);
+        var filterdata2 = this.data.filter((d) => d.Language === second);
+
+        this.legend.selectAll(".legend-item").remove();
+
+        // x축과 y축 설정
+        this.xAxisVal = xAxisVal;
+        this.yAxisVal = yAxisVal;
+
+        console.log(xAxisVal);
+        console.log(yAxisVal);
+
+        // x축과 y축 도메인 설정
+        this.xScale.domain([
+            d3.min([d3.min(filterdata1, (d) => d[xAxisVal]), d3.min(filterdata2, (d) => d[xAxisVal])]),
+            d3.max([d3.max(filterdata1, (d) => d[xAxisVal]), d3.max(filterdata2, (d) => d[xAxisVal])])
+        ]).range([0, this.width]);
+
+        this.yScale.domain([
+            d3.min([d3.min(filterdata1, (d) => d[yAxisVal]), d3.min(filterdata2, (d) => d[yAxisVal])]),
+            d3.max([d3.max(filterdata1, (d) => d[yAxisVal]), d3.max(filterdata2, (d) => d[yAxisVal])])
+        ]).range([this.height, 0]);
+
+        this.xAxis
+            .attr("transform", `translate(${this.margin.left}, ${this.margin.top + this.height})`)
+            .transition()
+            .call(d3.axisBottom(this.xScale));
+
+        this.yAxis
+            .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`)
+            .transition()
+            .call(d3.axisLeft(this.yScale));
 
 
-        console.log(language);
-        console.log(filteredData);
-
-        // Filter data if a specific language is selected
-        if (language !== "All") {
-            filteredData = filteredData.filter((d) => d.Language === language);
-        }
-        
-        //x축 y축 설정
-        this.x.domain([0, d3.max(filteredData, d => +d["Followers gained"])]);
-        this.y.domain([0, d3.max(filteredData, d => +d["Followers"])]);
-        
         // 데이터를 기반으로 점 추가
-        const dots = this.svg.selectAll(".dot")
-        .data(filteredData);
-        
-        dots.enter()
+        const dots1 = this.svg.selectAll(".dot1")
+        .data(filterdata1);
+
+        dots1.enter()
         .append("circle")
-        .attr("class", "dot")
-        .merge(dots)
-        .attr("cx", d => this.x(d["Followers gained"]))
-        .attr("cy", d => this.y(d["Followers"]))
+        .attr("class", "dot1")
+        .merge(dots1)
+        .transition()
+        .duration(500)
+        .attr("cx", (d) => this.xScale(d[xAxisVal]))
+        .attr("cy", (d) => this.yScale(d[yAxisVal]))
         .attr("r", 3)
-        .style("fill", d => this.color(d.Language));
-  
-        dots.exit().remove();
+        .style("fill", "skyblue");
+
+        dots1.exit().remove();
+        
+        const dots2 = this.svg.selectAll(".dot2")
+        .data(filterdata2);
+
+        dots2.enter()
+        .append("circle")
+        .attr("class", "dot2")
+        .merge(dots2)
+        .transition()
+        .duration(500)
+        .attr("cx", (d) => this.xScale(d[xAxisVal]))
+        .attr("cy", (d) => this.yScale(d[yAxisVal]))
+        .attr("r", 3)
+        .style("fill", "hotpink");
+        dots2.exit().remove();
+        const legendItems = this.legend.selectAll(".legend-item")
+        .data([first, second]);
+
+        const legendItemsEnter = legendItems.enter()
+        .append("g")
+        .attr("class", "legend-item")
+        .attr("transform", (d, i) => `translate(0, ${i * 20})`);
+
+        legendItemsEnter.append("circle")
+        .attr("cx", this.width + 20)
+        .attr("cy", 10)
+        .attr("r", 5)
+        .style("fill", (d, i) => i === 0 ? "skyblue" : "hotpink");
+
+        legendItemsEnter.append("text")
+        .attr("x", this.width + 30)
+        .attr("y", 14)
+        .text(d => d);
+
+        legendItemsEnter.merge(legendItems);
+
+        legendItems.exit().remove();
 
     }
 
